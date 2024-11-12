e23b761dc866b39c9bde7002a94fa25406b7ed32
bigmistqke <vandijckv@gmail.com>
2024-11-12 03:58:27 +0100

    use binary search in get-value-from-segments

--- src/lib/get-value-from-segments.ts
+++ src/lib/get-value-from-segments.ts
@@ -1,7 +1,54 @@
 import { Accessor } from 'solid-js'
-import { Segment } from 'solid-timeline/types'
+import { Segment, Vector } from 'solid-timeline/types'
 import { getLastArrayItem } from 'solid-timeline/utils/get-last-array-item'
 import { interpolateYAtX } from './interpolate-y-at-x'
+
+function binarySearchSegment(
+  segments: Array<Accessor<Segment>>,
+  time: number
+): Segment | null {
+  let low = 0
+  let high = segments.length - 1
+  while (low <= high) {
+    const mid = Math.floor((low + high) / 2)
+    const segment = segments[mid]()
+    if (time < segment.range[0]) {
+      high = mid - 1
+    } else if (time > segment.range[1]) {
+      low = mid + 1
+    } else {
+      return segment
+    }
+  }
+  return null
+}
+
+function binarySearchMap(
+  map: Vector[],
+  time: number
+): [Vector | null, Vector | null] {
+  let low = 0
+  let high = map.length - 1
+  while (low <= high) {
+    const mid = Math.floor((low + high) / 2)
+    const current = map[mid]
+    const next = map[mid + 1] || null
+
+    if (next && time > current.x && time < next.x) {
+      return [current, next]
+    }
+
+    if (time < current.x) {
+      high = mid - 1
+    } else if (time > current.x) {
+      low = mid + 1
+    } else {
+      // Exact match or we are on the edge
+      return [current, next]
+    }
+  }
+  return [null, null]
+}
 
 function closestPoint(segments: Array<Accessor<Segment>>, time: number) {
   if (segments.length === 0) {
@@ -19,31 +66,13 @@ function closestPoint(segments: Array<Accessor<Segment>>, time: number) {
     return [null, getLastArrayItem(max.map)]
   }
 
-  // NOTE:  this is not the fastest way of doing these lookups
-  //        maybe we can investigate another method (binary search p.ex)
-  const segment = segments.find((segment) => {
-    return segment().range[0] <= time && time <= segment().range[1]
-  })?.()
-
+  const segment = binarySearchSegment(segments, time)
   if (!segment) {
     console.error('This should not happen')
     return [null, null]
   }
 
-  // NOTE:  this is not the fastest way of doing these lookups
-  //        maybe we can investigate another method (binary search p.ex)
-  for (let i = 0; i < segment.map.length; i++) {
-    const current = segment.map[i]
-    const next = segment.map[i + 1]
-
-    if (!next) continue
-
-    if (current.x <= time && time <= next.x) {
-      return [current, next]
-    }
-  }
-
-  return [null, null]
+  return binarySearchMap(segment.map, time)
 }
 
 export function getValueFromSegments(
