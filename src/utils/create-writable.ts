cb15129f697a26fc134c26610cc3833ba2e18827
bigmistqke <vandijckv@gmail.com>
2024-11-14 01:56:14 +0100

    update create-writable to higher order signal
    taken from https://www.youtube.com/live/jfZdl3QckMM?si=xMLg3Jttg1TxKpoF&t=5765 thanks milo

--- src/utils/create-writable.ts
+++ src/utils/create-writable.ts
@@ -1,7 +1,6 @@
-import { Accessor, createRenderEffect, createSignal } from 'solid-js'
+import { Accessor, createMemo, createSignal, Setter } from 'solid-js'
 
 export function createWritable<T>(source: Accessor<T>) {
-  const [signal, setSignal] = createSignal<T>(null!)
-  createRenderEffect(() => setSignal(source() as any))
-  return [signal, setSignal] as const
+  const memo = createMemo(() => createSignal(source))
+  return [() => memo()[0](), (v) => memo()[1](v)] as [Accessor<T>, Setter<T>]
 }
