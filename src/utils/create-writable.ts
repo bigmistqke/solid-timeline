de701795b2d0ded1f462881c820a4c6edb6563a3
bigmistqke <vandijckv@gmail.com>
2024-11-15 17:52:16 +0100

    fix createWritable

--- src/utils/create-writable.ts
+++ src/utils/create-writable.ts
@@ -1,6 +1,6 @@
 import { Accessor, createMemo, createSignal, Setter } from 'solid-js'
 
 export function createWritable<T>(source: Accessor<T>) {
-  const memo = createMemo(() => createSignal(source))
+  const memo = createMemo(() => createSignal(source()))
   return [() => memo()[0](), (v) => memo()[1](v)] as [Accessor<T>, Setter<T>]
 }
